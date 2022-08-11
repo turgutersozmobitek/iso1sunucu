ad7d0b343d18e5656c6e397e328c1577afaa26f3
turgutersozmobitek <98022847+turgutersozmobitek@users.noreply.github.com>
2022-08-11 11:21:25 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,50 @@
+#bir oluşturalım 
+
+resource "vcd_vapp_org_network" "vappOrgNet" {
+  org = var.org_name
+  vdc = var.vdc_name
+  vapp_name = var.vapp_name
+
+  # Comment below line to create an isolated vApp network
+  org_network_name = var.org_network_name
+}
+
+
+resource "vcd_vapp_vm" "web1" {
+  vapp_name        = var.vapp_name
+  name             = var.vm_name
+  catalog_name     = var.catalog_name
+  os_type          = var.os_type
+  hardware_version = "vmx-14"
+  boot_image       = var.boot_image
+  memory           = var.vm_memory
+  cpus             = var.vm_cpu
+  cpu_cores        = var.vm_cpu_core
+  cpu_hot_add_enabled=true
+  memory_hot_add_enabled=true
+  power_on=true
+
+    override_template_disk {
+    bus_type        = "paravirtual"
+    size_in_mb      = var.vm_disk_size
+    bus_number      = 0
+    unit_number     = 0
+    iops            = 0
+  }
+
+
+  network {
+    type               = "org"
+    name               = var.org_network_name
+    ip_allocation_mode = "MANUAL"
+    ip                 = var.vm_ip
+    adapter_type       = "VMXNET3"
+    is_primary         = true
+  }
+
+   customization{
+      enabled=false
+    }
+
+
+}
---
+++ variables.tf
@@ -0,0 +1,104 @@
+# vCloud Director Organization Variables
+variable "cd_username" {
+    type        = string
+    description = "vCD UserName"
+}
+variable "cd_password" {
+    type        = string
+    description = "vCD Password"
+}
+
+variable "cd_vdcadres" {
+    type        = string
+    description = "vCD Adresi"
+}
+
+variable "org_name" {
+    type        = string
+    description = "Organizasyon İsmi:"
+    default     = "list"
+}
+
+variable "vdc_name" {
+    type        = string
+    description = "VDC Adı:"
+    default     = "list"
+
+}
+
+
+variable "vapp_name" {
+    type        = string
+    description = "VAPP Adı:"
+    default     = "list"
+}
+
+
+variable "vm_cpu" {
+    type        = string
+    description = "CPU:"
+    default     = "list"
+}
+
+variable "vm_cpu_core" {
+    type        = string
+    description = "CPU Core:"
+    default     = "list"
+}
+
+
+variable "vm_memory" {
+    type        = string
+    description = "Memory:"
+    default     = "list"
+}
+
+variable "vm_disk_size" {
+    type        = string
+    description = "Disk:"
+    default     = "list"
+}
+
+
+variable "org_network_name" {
+    type        = string
+    description = "Network Adı:"
+    default     = "list"
+}
+
+
+variable "vm_description" {
+    type        = string
+    description = "MANAGED/UNMANAGED:"
+    default     = "list"
+}    
+
+variable "catalog_name" {
+    type        = string
+    description = "Catalog:"
+    default     = "list"
+}
+
+variable "os_type" {
+  type        = string
+  description = "OS Tipi:"
+  default     = "list"
+}
+
+
+variable "boot_image" {
+  type        = string
+  description = "Imaj:"
+  default     = "list"
+}
+
+
+variable "vm_ip" {
+    type        = string
+    description = "IP Adresi:"
+}
+
+variable "vm_name" {
+    type        = string
+    description = "Sunucu Adı:"
+}
