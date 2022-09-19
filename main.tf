a6ec4a3237be6e5573c8097461e368fe1579a0c3
turgutersozmobitek <98022847+turgutersozmobitek@users.noreply.github.com>
2022-09-19 16:04:49 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,6 @@ resource "vcd_independent_disk" "myNewIndependentDisk" {
   name            = var.vm_disk_name
   size_in_mb      = var.vm_disk_size
   bus_type        = "SCSI"
-  bus_sub_type    = "SCSI"
 }
 
 
