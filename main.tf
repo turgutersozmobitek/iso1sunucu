14c933fd83c6d7ecef2247783e02207b59efbbd4
turgutersozmobitek <98022847+turgutersozmobitek@users.noreply.github.com>
2022-09-26 11:56:29 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,17 +19,6 @@ resource "vcd_vapp_org_network" "vappOrgNet" {
  # bus_sub_type = "VirtualSCSI"
 #}
 
-resource "vcd_vm_internal_disk" "disk1" {
-  vapp_name       = var.vapp_name
-  vm_name         = var.vm_name
-  bus_type        = "paravirtual"
-  size_in_mb      = var.vm_disk_size
-  bus_number      = 0
-  unit_number     = 1
-  allow_vm_reboot = true
-  depends_on      = ["vcd_vapp_vm.web1"]
-}
-
 
 
 
@@ -71,4 +60,15 @@ resource "vcd_vapp_vm" "web1" {
     }
 
 
+}
+
+resource "vcd_vm_internal_disk" "disk1" {
+  vapp_name       = var.vapp_name
+  vm_name         = var.vm_name
+  bus_type        = "paravirtual"
+  size_in_mb      = var.vm_disk_size
+  bus_number      = 0
+  unit_number     = 1
+  allow_vm_reboot = true
+  depends_on      = ["vcd_vapp_vm.web1"]
 }
